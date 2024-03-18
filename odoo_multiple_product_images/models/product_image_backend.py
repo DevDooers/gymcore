ee6c5be52ff3075c75dda23c1fc8275912facee1
Desarrollo1tu3w <edisonsjumbo@gmail.com>
2024-03-18 11:30:39 -0500

    rest_api: show images content

--- odoo_multiple_product_images/models/product_image_backend.py
+++ odoo_multiple_product_images/models/product_image_backend.py
@@ -182,7 +182,7 @@ class ProductProduct(models.Model):
             for image in product.product_tmpl_id.template_image_backend_ids:
                 template_images.append({
                     'id': image.id,
-                    'name': image.name
+                    'image_386': image.image_medium
                 })
             product.related_template_image_backend_ids = template_images
 
