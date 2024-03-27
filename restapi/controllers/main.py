93fe51391dc1fd65fccde393a79e972ade2a321b
Desarrollo1tu3w <edisonsjumbo@gmail.com>
2024-03-27 15:38:39 -0500

    login

--- restapi/controllers/main.py
+++ restapi/controllers/main.py
@@ -651,5 +651,5 @@ class RestApi(http.Controller):
             "access_token": access_token,
             "access_token_validity": access_token_validity,
             "token_type": 'Basic',
-            'refresh_token': auth_auth.refresh_token
+            #"refresh_token": auth_auth.refresh_token
             })
