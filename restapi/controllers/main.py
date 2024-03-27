f261812b7372a961afccdda09a7d4e15ddec61b3
Desarrollo1tu3w <edisonsjumbo@gmail.com>
2024-03-27 15:31:32 -0500

    login

--- restapi/controllers/main.py
+++ restapi/controllers/main.py
@@ -640,16 +640,16 @@ class RestApi(http.Controller):
         partner_id = obj.search([('email', '=', kwargs.get('email')), ('password', '=', kwargs.get('password'))], limit=1)
         if not partner_id:
             return self.get_response(401, '401', {"code": 401, "message": "Invalid Credentials."})
-        #auth = request.env['auth.auth'].sudo()
+        auth = request.env['auth.auth'].sudo()
         access_token = auth.generate_token()
-        #access_token_validity = datetime.now() + timedelta(minutes=30)
-        #auth_auth.basic_access_token_ids = [(0, 0, {'access_token': access_token, 'auth_id': False, 'access_token_validity': access_token_validity})]
+        access_token_validity = datetime.now() + timedelta(minutes=30)
+        auth_auth.basic_access_token_ids = [(0, 0, {'access_token': access_token, 'auth_id': False, 'access_token_validity': access_token_validity})]
         return self.get_response(200, '200', {
             "id": partner_id.id,
             "display_name": partner_id.name,
             "image_512": '',
             "access_token": access_token,
-            #"access_token_validity": access_token_validity,
-            #"token_type": 'Basic',
-            #'refresh_token': auth_auth.refresh_token
+            "access_token_validity": access_token_validity,
+            "token_type": 'Basic',
+            'refresh_token': auth_auth.refresh_token
             })
