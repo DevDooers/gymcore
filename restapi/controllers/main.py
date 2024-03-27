12bdace24c1328626393495723b5be60a27870de
Desarrollo1tu3w <edisonsjumbo@gmail.com>
2024-03-27 16:29:46 -0500

    final api

--- restapi/controllers/main.py
+++ restapi/controllers/main.py
@@ -641,9 +641,10 @@ class RestApi(http.Controller):
         if not partner_id:
             return self.get_response(401, '401', {"code": 401, "message": "Invalid Credentials."})
         auth = request.env['auth.auth'].sudo()
+        auth_auth = auth.search([('access_type', '=', 'basic')], limit=1, order='id desc')
         access_token = auth.generate_token()
         access_token_validity = datetime.now() + timedelta(minutes=30)
-        auth.basic_access_token_ids = [(0, 0, {'access_token': access_token, 'auth_id': False, 'access_token_validity': access_token_validity})]
+        auth_auth.basic_access_token_ids = [(0, 0, {'access_token': access_token, 'auth_id': False, 'access_token_validity': access_token_validity})]
         return self.get_response(200, '200', {
             "id": partner_id.id,
             "display_name": partner_id.name,
@@ -651,5 +652,5 @@ class RestApi(http.Controller):
             "access_token": access_token,
             "access_token_validity": access_token_validity,
             "token_type": 'Basic',
-            "refresh_token": auth.refresh_token,
+            "refresh_token": auth_auth.refresh_token,
             })
