b9344be6c432667d8c01edeb2c13ca2edd7b33d9
Desarrollo1tu3w <edisonsjumbo@gmail.com>
2024-03-27 17:01:43 -0500

    rest_api

--- restapi/controllers/main.py
+++ restapi/controllers/main.py
@@ -648,8 +648,8 @@ class RestApi(http.Controller):
         auth_auth.basic_access_token_ids = [(0, 0, {'access_token': access_token, 'auth_id': False, 'access_token_validity': access_token_validity})]
         return self.get_response(200, '200', {
             "id": partner_id.id,
-            #"display_name": partner_id.name,
-            #"image_512": '',
+            "display_name": partner_id.name,
+            "image_512": '',
             #"access_token": access_token,
             #"access_token_validity": access_token_validity,
             #"token_type": 'Basic',
